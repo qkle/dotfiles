76fc9894c225c34aa67cab50a337830b778ec887
qkle <quockhanh.dn@gmail.com>
2025-04-22 23:06:54 +1000

    Install which-key plugin & clean up comments

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -1,4 +1,3 @@
--- check if linking works
 -- Set <space> as the leader key
 -- See `:help mapleader`
 -- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -136,176 +135,25 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
-        -- NOTE: Plugins can be added with a link (or for a github repo: `owner/repo' link).
-        -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+    -- NOTE: Plugins can be added with a link (or for a github repo: `owner/repo' link).
+    -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
 })
-
-        -- NOTE: Plugins can also be added by using a table,
-        -- with the first argument being the link and the following
-        -- keys can be used to configure plugin behavior/loading/etc.
-        --
-        -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
-        --
-
-        -- Alternatively, use `config = function() ... end` for full control over the configuration.
-        -- If you prefer to call `setup` explicitly, use:
-        -- {
-        --      'lewis6991/gitsigns.nvim',
-        --      config = function()
-        --              require('gitsigns').setup({
-        --                      -- Your gitsigns configuration herer
-        --              })
-        --      end,
-        -- }
-        --
-        -- Here is a more advanced exmple where we pass configuration
-        -- options to `gitsigns.nvim`.
-        --
-        -- See `:help gitsigns` to understand what the configuration keys do
-        -- { -- Adds git related signs to the gutter, as well as utilities for managing changes
-        --      'lewis6991/gitsigns.nvim',
-        --      opts = {
-        --              signs = {
-        --                      add = { text = '+' },
-        --                      change = { text = '~' },
-        --                      delete = { text = '_' },
-        --                      topdelete = { text = '-' },
-        --                      changedelete = { text = '~' },
-        --              },
-        --      },
-        -- },
-
-        -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-        --
-        -- This is often very useful to both group configuration, as well as handle
-        -- lazy loading plugins that don't need to be loaded immediately at startup.
-        --
-        -- For example, in the following configuration, we use:
-        -- event = 'VimEnter'
-        --
-        -- which loads which-key before all the UI elements are loaded. Events can be
-        -- normal autocommands events (`:help autocmd-events`).
-        --
-        -- Then, because we use the `opts` key (recommended), the configuration runs
-        -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
-
-        -- { -- Useful plugin to show you pending keybinds.
-        --      'folke/which-key.nvim',
-        --      event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-        --      opts = {
-        --              -- delay between pressing a key and opening which-key (milliseconds)
-        --              -- this setting is independent of vim.opt.timeoutlen
-        --              delay = 0,
-        --              icons = {
-        --                      -- set icon mappings to true if you have a Nerd Font
-        --                      mappings = vim.g.have_nerd_font,
-        --                      -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-        --                      -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
-        --                      keys = vim.g.have_nerd_font and {} or {
-        --                              Up = '<Up> ',
-        --                              Down = '<Down> ',
-        --                              Left = '<Left> ',
-        --                              Right = '<Right> ',
-        --                              C = '<C-…> ',
-        --                              M = '<M-…> ',
-        --                              D = '<D-…> ',
-        --                              S = '<S-…> ',
-        --                              CR = '<CR> ',
-        --                              Esc = '<Esc> ',
-        --                              ScrollWheelDown = '<ScrollWheelDown> ',
-        --                              ScrollWheelUp = '<ScrollWheelUp> ',
-        --                              NL = '<NL> ',
-        --                              BS = '<BS> ',
-        --                              Space = '<Space> ',
-        --                              Tab = '<Tab> ',
-        --                              F1 = '<F1>',
-        --                              F2 = '<F2>',
-        --                              F3 = '<F3>',
-        --                              F4 = '<F4>',
-        --                              F5 = '<F5>',
-        --                              F6 = '<F6>',
-        --                              F7 = '<F7>',
-        --                              F8 = '<F8>',
-        --                              F9 = '<F9>',
-        --                              F10 = '<F10>',
-        --                              F11 = '<F11>',
-        --                              F12 = '<F12>',
-        --                      },
-        --              },
-
-                        -- Document existing key chains
-        --              spec = {
-        --                      { '<leader>s', group = '[S]earch' },
-        --                      { '<leader>t', group = '[T]oggle' },
-        --                      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-        --              },
-        --      },
-        -- },
-
-        -- NOTE: Plugins can specify dependencies.
-        --
-        -- The dependencies are proper plugin specifications as well - anything
-        -- you do for a plugin at the top level, you can do for a dependency.
-        --
-        -- Use the `dependencies` key to specify the dependencies of a particular plugin
-
---      { -- Fuzzy Finder (files, lsp, etc)
---              'nvim-telescope/telescope.nvim',
---              event = 'VimEnter',
---              dependencies = {
---                      'nvim-lua/plenary.nvim',
---                      { -- If encountering errors, see telescope-fzf-native README for installation instructions
---                              'nvim-telescope/telescope-fzf-native.nvim',
---
---                              -- `build` is used to run some command when the plugin is installed/updated.
---                              -- This is only run then, not every time neovim starts up.
---                              build = 'make',
---
---                              -- `cond` is a condition used to determine whether this plugin should be
---                              -- installed and loaded.
---                              cond = function()
---                                      return vim.fn.executable 'make' == 1
---                              end,
---                      },
---                      { 'nvim-telescope/telescope-ui-select.nvim' },
---
---                      -- Useful for getting pretty icons, but requires a Nerd Font.
---                      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
---              },
---              config = function()
---                      -- Telescope is a fuzzy finder that comes with a lot of different things that
---                      -- it can fuzzy find! It's more than just a "file finder", it can search
---                      -- many different aspects of Neovim, your workspace, LSP, and more!
---                      --
---                      -- The easiest way to use Telescope, is to start by doing something like:
---                      -- :Telescope help_tags
---                      --
---                      -- After running this command, a window will open up and you're able to
---                      -- type in the prompt window. You'll see a list of `help_tags` options and
---                      -- a corresponding preview of the help.
---                      --
---                      -- Two important keymaps to use while in Telescope are:
---                      --      - Insert mode: <C-/>
---                      --      - Normal mode: ?
---                      --      
---                      -- This opens a window that shows you all of the keymaps for the current
---                      -- Telescope picker. This is really useful to discover what Telescope can
---                      -- do as well as how to actually do it!
---
---                      -- [[ Configure Telescope ]]
---                      -- See `:help telescope` and `:help telescope.setup()`
---                      require('telescope').setup {
---                              -- You can put your default mappings / updates / etc. in here
---                              -- All the info you're looking for is in `:help telescope.setup()`
---                              --
---                              -- defaults = {
---                              --      mappings = {
---      spec = {
---              -- add your plugins here
---      },
---      -- Configure any other settings here. See the documentation for more details.
---      -- colorscheme that will be used when installing plugins.
---      install = { colorscheme = { "habamax" } },
---      -- automatically check for plugin updates
---      checker = { enabled = true },
--- })
